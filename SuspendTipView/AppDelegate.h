3d459291c735a39f0bf534fa17f5bedeb6ecd4f3
NSSONGMENG <740621245@qq.com>
2017-09-12 22:48:21 +0800

    add new target

---
+++ SuspendTipView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SuspendTipView
+//
+//  Created by songmeng on 17/9/12.
+//  Copyright © 2017年 songmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SuspendTipView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SuspendTipView
+//
+//  Created by songmeng on 17/9/12.
+//  Copyright © 2017年 songmeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
